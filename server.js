ccaf9aafb8651f3a6d020ce48d228111209f8549
Hannah Catherine Trask <hctrask@gmail.com>
2020-11-19 15:08:06 -0700

    proxy working for a fixed route

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
-const morgan = require('morgan');
 const cors = require('cors');
+const fetch = require('node-fetch');
 
-app.use(morgan('morgan'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(cors());
-
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	next();
 });
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/', (req, res) => {
-    // read query parameters, not sure where these come from
-    // consider token
-	const lat = req.query['lat'];
-    const lng = req.query['lng'];
-	const url = `http://api.powderlin.es/closest_stations?lat=${lat}&lng=${lng}&data=true&days=5&count=5`;
-	request(url).pipe(res);
-})
+app.get('/', async (req, res) => {
+	const response = await fetch('http://api.powderlin.es/closest_stations?lat=43.4695&lng=-110.7892&data=true&days=5&count=5');
+	const data = await response.json();
+	res.json(await data);
+});
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
