b2650caa64f652f53eeea71b42425b27afb39676
Hannah Catherine Trask <hctrask@gmail.com>
2020-11-20 09:46:00 -0700

    proxy server, query route

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', async (req, res) => {
 	const response = await fetch('http://api.powderlin.es/closest_stations?lat=43.4695&lng=-110.7892&data=true&days=5&count=5');
 	const data = await response.json();
 	res.json(await data);
-});
+}); 
 
 // this route will be for requesting lat and long data
 app.get('/request/:lat/:long', async (req, res) => {
