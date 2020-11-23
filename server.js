0044669f451d52833e1e4fce65de65beba9c32fe
Hannah Catherine Trask <hctrask@gmail.com>
2020-11-23 07:54:48 -0700

    route for triple info added

--- server.js
+++ server.js
@@ -21,21 +21,31 @@ app.use(express.json());
 app.use(morgan('tiny')); //logging
 
 //controllers
-const imageController = require('./controllers/image')
-app.use('/avysavvy', imageController)
-
+const imageController = require('./controllers/image');
+app.use('/avysavvy', imageController);
 
 app.get('/', async (req, res) => {
-	const response = await fetch('http://api.powderlin.es/closest_stations?lat=43.4695&lng=-110.7892&data=true&days=5&count=5');
+	const response = await fetch(
+		'http://api.powderlin.es/closest_stations?lat=43.4695&lng=-110.7892&data=true&days=5&count=5'
+	);
 	const data = await response.json();
 	res.json(await data);
-}); 
+});
 
 // this route will be for requesting lat and long data
 app.get('/request/:lat/:long', async (req, res) => {
-    const { lat, long } = req.params;
+	const { lat, long } = req.params;
 	const response = await fetch(
 		`http://api.powderlin.es/closest_stations?lat=${lat}&lng=${long}&data=true&days=5&count=5`
+	);
+	const data = await response.json();
+	res.json(await data);
+});
+
+app.get('/request/:triplet', async (req, res) => {
+	const { triplet } = req.params;
+	const response = await fetch(
+		`http://api.powderlin.es/station/${triplet}?days=5`
 	);
 	const data = await response.json();
 	res.json(await data);
